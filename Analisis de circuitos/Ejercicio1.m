f85af259733c6b3d498dde102fa8b69445bb9080
luis-pabl <luispablito1terran@gmail.com>
2021-02-19 17:23:23 -0600

    Se les coloco titulos y se separaron las graficas

--- Analisis de circuitos/Ejercicio1.m
+++ Analisis de circuitos/Ejercicio1.m
@@ -1,5 +1,5 @@
 %Ejercicio 1
-FM=1000 %Frecuencia de Muestreo de la Simulacion
+FM=1000; %Frecuencia de Muestreo de la Simulacion
 %Teorema de Nyquis o Teroema de Muetreo:
 %Para muetrear una señal de frecuencia f se requiere una frecuencia F de muetreo de almenos 2f, es decir F=2f
 %Para muetrear una señal de periodo t, se requiere un tiempo de muetreo de alemnos t/2
@@ -14,7 +14,10 @@ R3=20;
 Req=R1+R2+R3;
 i_x=V_x/Req;
 Vr1=i_x*R1;
+Vr2=i_x*R2;
+Vr3=i_x*R3;
+%Ley de Kirchhoff
+Vt=Vr1+Vr2+Vr3;
 
-
-plot(t,V_x,t,Vr1);
+plot(t,V_x,t,Vr1,t,Vr2,t,Vr3,t,Vt);
 grid on
---
+++ Analisis de circuitos/Ejercicio2.m
@@ -0,0 +1,63 @@
+%Ejercicio 2
+FM=1000; %Frecuencia de Muestreo de la Simulacion
+%Teorema de Nyquis o Teroema de Muetreo:
+%Para muetrear una señal de frecuencia f se requiere una frecuencia F de muetreo de almenos 2f, es decir F=2f
+%Para muetrear una señal de periodo t, se requiere un tiempo de muetreo de alemnos t/2
+%%Formulas 
+%%V=IR
+
+t=0:1/FM:.1;
+A=12;
+f=60;
+V_x=A*sin(2*pi*f*t);
+
+R1=100;
+R2=220;
+R3=120;
+R4=1000;
+R5=2200;
+R6=1000;
+
+Req1=1/(1/R2+1/R4+1/R5)
+Req2=1/(1/R3+1/R6)
+ReqT=Req1+Req2+R1
+I_x=V_x/ReqT;
+Vr1=I_x*R1;
+VReq2=I_x*Req2;
+VReq1=I_x*Req1;
+
+Vt_x=Vr1+VReq1+VReq2;
+%figure 1
+subplot(2,2,1)
+plot(t,V_x);
+title('Señal de voltaje normal')
+xlabel('t')
+ylabel('V_x')
+%figure 2
+subplot(2,2,2)
+plot(t,Vr1);
+title('Señal del voltaje de R1')
+xlabel('t')
+ylabel('Vr1')
+%figure 3
+subplot(2,2,3)
+plot(t,Vt_x);
+title('Señal de voltaje suma')
+xlabel('t')
+ylabel('Vt_x')
+%figure 4
+subplot(2,2,4)
+plot(t,VReq1);
+title('Señal de Voltaje de R5')
+xlabel('t')
+ylabel('VReq1')
+
+figure 2
+plot(t,V_x,t,Vr1,t,Vt_x,t,VReq1);
+title('Señales de Voltajes')
+xlabel('t')
+ylabel('V_x')
+
+
+%%Poner detalles a las graficas
+%%Cada una separada
