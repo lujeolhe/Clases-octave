a52fe13a3c001c1269faab11f0f1e5fc3dc8fa32
luis-pabl <luispablito1terran@gmail.com>
2021-06-13 10:13:19 -0500

    Se realizo el metodo del trapecio bien

--- Analisis de circuitos/Proyecto Final/Trapecio.m
+++ Analisis de circuitos/Proyecto Final/Trapecio.m
@@ -1,14 +1,33 @@
 %Metodo de Integracion de Trapecio
+close all
 h=1;
-x=0:1:5
-n=6;
+x=0:h:5;
+y=zeros(1,length(x)-1);
+y_x=zeros(1,length(x));
+n=length(x);
 A=0;
+y_x(1)=-0.2018+exp(0);
 for i=1:n
-  for j=i-1:i-1
-    if i<=5
-    b=-0.2018+sqrt(x(i+1).^2+9)/3
-    a=-0.2018+sqrt(x(j+1).^2+9)/3
-    A=(x(i+1)-x(j+1))+(b+a)/2
+    if i<n
+    b=x(i+1)/(x(i+1).^2+9);
+    a=x(i)/(x(i).^2+9);
+    A=A+(x(i+1)-x(i))*((b+a)/2)
     endif
-  end
+  if i>1
+    y_x(i)=-0.2018+exp(A);
+  endif
 end
+x_2=0:1/1000:5;
+  y_2=sqrt(x_2.^2+9)/3-0.2018;
+  plot(x,y_x,x_2,y_2);
+  hold on
+  y0=zeros(1,length(x));
+  yp=zeros(1,length(x));
+
+  y0(1)=-0.2018;
+  yp(1)=0;
+  for i=2:n
+    y0(i)=y0(i-1)+h*yp(i-1);
+    yp(i)=-x(i)/(x(i).^2+9)*(y0(i));
+  endfor
+  plot(x,y0);
