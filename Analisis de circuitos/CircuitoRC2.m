5556409aa0b9b023d36bd764939fe607be9caa15
luis-pabl <luispablito1terran@gmail.com>
2021-07-17 13:46:36 -0500

    Se realizo la tarea del 10 y parte de la del 11 de julio

--- Analisis de circuitos/CircuitoRC2.m
+++ Analisis de circuitos/CircuitoRC2.m
@@ -1,12 +1,15 @@
 %Circuito RC en DC
-FM=1000; %Frecuencia de Muestreo de la Simulacion
+close all
+FM=10000; %Frecuencia de Muestreo de la Simulacion
+
 t=0:1/FM:1;
-f=10;
+f=60;
 V=5;
 R1=100;
-R2=10;
-C=0.02;
-Vc=-(V*R2)/(R1+R2)*exp(((R1+R2)/(C*R1*R2))*t)+(V*R2)/(R1+R2);
+R2=33.3;
+C=5*10^-6;
+
+Vc=-(V*R2)/(R1+R2)*exp(-((R1+R2)/(C*R1*R2))*t)+(V*R2)/(R1+R2);
 I1=(V-Vc)/R1;
 Vr1=I1*R1;
 VT=Vr1+Vc-V;
@@ -18,7 +21,7 @@ ylabel('V_x')
 legend('V(t)','V_{R1}','V_{C}');
 %Circuito RC en DA
 figure 2
-t2=0:1/FM:0.5;
+t2=0:1/FM:0.02;
 K=(R1+R2)/(C*R2*R1);
 w=2*pi*f;
 V2=5*sin(w*t2);
@@ -28,7 +31,7 @@ Vc2=a*exp(-K*t2)+b*sin(w*t2)-b*cos(w*t2);
 I2=(V2-Vc2)/R1;
 Vr1_2=R1*I2;
 VT2=Vr1_2+Vc2-V2;
-plot(t2,V2,t2,Vr1_2,t2,Vc2);
+plot(t2,V2,t2,Vr1_2,t2,Vc2,t2,VT2);
 title('Señales de Voltajes')
 xlabel('t')
 ylabel('V_x')
